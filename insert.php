d65ca690d43242e5726c0b839028a6f64a9b5969
KaraboMokotedi <158720029+KaraboMokotedi@users.noreply.github.com>
2024-04-15 22:24:05 +0200

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,21 @@
+<?php
+// Database connection
+$conn = new COM("ADODB.Connection");
+$connStr = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=customers.accdb;";
+$conn->open($connStr);
+
+// Get form data
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$gender = $_POST['gender'];
+// Insert data into database
+$sql = "INSERT INTO table (firstname, lastname,gender) VALUES ('$firstname', '$lastname','$gender')";
+$conn->execute($sql);
+
+// Close connection
+$conn->close();
+
+// Redirect back to the form page
+header("Location: index.html");
+exit();
+?>
